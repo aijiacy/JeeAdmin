9a7e8be848dbb7cf6e28d5e73c4160b45ef41636
caoyong <cymsn81@126.com>
2015-04-18 17:24:21 +0800

    增加日志拦截器

---
+++ src/main/java/com/jeasyframeworks/system/interceptor/LogInterceptor.java
@@ -0,0 +1,32 @@
+package com.jeasyframeworks.system.interceptor;
+
+import org.apache.shiro.SecurityUtils;
+
+import com.jeasyframeworks.system.model.Account;
+import com.jeasyframeworks.system.model.Log;
+import com.jfinal.aop.Interceptor;
+import com.jfinal.core.ActionInvocation;
+import com.jfinal.core.Controller;
+
+public class LogInterceptor implements Interceptor{
+
+	@Override
+	public void intercept(ActionInvocation ai) {
+		Controller ctrl = ai.getController();
+		Log log = new Log();
+		log.put(Log.OP_NAME, ctrl.getClass().getSimpleName());
+		log.put(Log.OP_ACTION, ai.getActionKey());
+		if(SecurityUtils.getSubject().isAuthenticated()){
+			String opUser = ((Account)SecurityUtils.getSubject().getPrincipal()).getStr(Account.NAME) + "(" + SecurityUtils.getSubject().getSession().getHost()+ ")";
+			log.put(Log.OP_USER, opUser);
+		} else {
+			String hostAddr = ai.getController().getRequest().getRemoteHost();
+			log.put(Log.OP_USER, "NONE(" + hostAddr + ")");
+		}
+		log.put(Log.OP_DESC, ctrl.getClass().getName());
+		Log.dao.setAttrs(log);
+		Log.dao.save();
+		ai.invoke();
+	}
+
+}
