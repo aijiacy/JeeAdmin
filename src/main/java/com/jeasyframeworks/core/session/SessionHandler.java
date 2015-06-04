5985a584d8bacdd8776f17f0f838d7f9a22e80b7
aijiacy <cymsn81@126.com>
2015-06-04 19:48:51 +0800

    首页
    Signed-off-by: aijiacy <cymsn81@126.com>

--- src/main/java/com/jeasyframeworks/core/session/SessionHandler.java
+++ src/main/java/com/jeasyframeworks/core/session/SessionHandler.java
@@ -2,14 +2,10 @@ package com.jeasyframeworks.core.session;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.jfinal.handler.Handler;
-import com.jfinal.log.Logger;
 
 public class SessionHandler extends Handler {
-
-	private static final Logger logger = Logger.getLogger(SessionHandler.class);
 	
 	/**
 	 * 去掉 URL中的;JSEESIONID=××××××
@@ -17,19 +13,12 @@ public class SessionHandler extends Handler {
 	@Override
 	public void handle(String target, HttpServletRequest request, HttpServletResponse response,
 			boolean[] isHandled) {
-		if(request.isRequestedSessionIdFromURL()){
-			HttpSession session = request.getSession(false);
-			if(null != session){
-				session.invalidate();
-			}
-			target = request.getServletPath();
-			if(logger.isDebugEnabled()){
-				logger.debug("sessionIdVaild:" + request.isRequestedSessionIdValid());
-				logger.debug("sessionIdFromCookie:" + request.isRequestedSessionIdFromCookie());
-				logger.debug("sessionIdFromURL:" + request.isRequestedSessionIdFromURL());
-				logger.debug("ActionKey:" + target);
-			}
+		String sessionIdParam = ";JEESSIONID=";
+		int idx = target.lastIndexOf(sessionIdParam);
+		if(idx == -1){
+			idx = target.lastIndexOf(sessionIdParam.toLowerCase());
 		}
+		target = idx == -1 ? target : target.substring(0, idx);
 		nextHandler.handle(target, request, response, isHandled);
 	}
 
